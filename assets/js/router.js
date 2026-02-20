aeeeb3d1f3c1a4848c06d06ab21e9c963f379bf9
Vampire Squad <vampiresquad.org@gmail.com>
2026-02-21 01:47:55 +0600

    Update router.js

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,1 +1,18 @@
+const routes = {
+  "": renderHome,
+  "#home": renderHome,
+  "#quran": renderQuran,
+  "#hadith": renderHadith,
+  "#pillars": renderPillars,
+  "#ramadan": renderRamadan,
+  "#dua": renderDua
+};
 
+function router() {
+  const hash = window.location.hash;
+  const route = routes[hash] || renderHome;
+  route();
+}
+
+window.addEventListener("hashchange", router);
+window.addEventListener("DOMContentLoaded", router);
