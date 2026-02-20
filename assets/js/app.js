c11399b9adb5191bc34dbb6acb2a1f2beca7c091
Vampire Squad <vampiresquad.org@gmail.com>
2026-02-21 02:41:09 +0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -106,7 +106,7 @@ async function loadSurah(fileName) {
     const response = await fetch(`data/quran/${fileName}`);
     const data = await response.json();
 
-    let content = `
+let content = `
   <div style="text-align:center; margin-bottom:30px;">
     <h2>${data.name}</h2>
     <h3 style="font-family:'Amiri'; direction:rtl;">${data.arabic_name}</h3>
@@ -128,7 +128,7 @@ async function loadSurah(fileName) {
       `;
     });
 
-    content += `<br><button class="button" onclick="location.hash='#quran'">← সূরা তালিকায় ফিরে যান</button>`;
+content += `<br><button class="button" onclick="location.hash='#quran'">← সূরা তালিকায় ফিরে যান</button>`;
 
   } catch (error) {
     app.innerHTML = `<p>এই সূরাটি এখনো যুক্ত হয়নি।</p>
